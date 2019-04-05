6a0ec2d20cf5afc0751bf54ba41610e4f58d3aac
Gerard Downes <gdownes@gmail.com>
2019-04-05 17:12:51 +0100

    Render and style contact form input field errors

--- src/features/Contact/components/ContactForm.tsx
+++ src/features/Contact/components/ContactForm.tsx
@@ -1,6 +1,6 @@
-import {Field, FormErrors, InjectedFormProps, reduxForm} from 'redux-form';
+import { Field, FormErrors, InjectedFormProps, reduxForm, WrappedFieldProps } from 'redux-form';
 import * as React from 'react';
-import {default as initialState} from '../index-state';
+import { default as initialState } from '../index-state';
 import './ContactForm-styles.sass';
 
 export interface ContactFormData {
@@ -14,37 +14,61 @@ export interface ContactFormProps {
   submitContactForm: (values: ContactFormData, {}, props: ContactFormProps) => void;
 }
 
+type InputProps =  {
+  placeholder: string;
+  rows: number;
+} & WrappedFieldProps;
+
 const contactForm: React.FunctionComponent<ContactFormProps & InjectedFormProps<ContactFormData, ContactFormProps>> = ({
    handleSubmit,
    submitContactForm,
    pristine,
    submitting,
+   invalid,
 }) => (
   <form className="contact-form" onSubmit={handleSubmit(submitContactForm)}>
     <div className="detail">
-      <Field name="name" component="input" placeholder="Your Name:" type="text" />
-      <Field name="telephone" component="input" placeholder="Telephone:" type="text" />
-      <Field name="email" component="input" placeholder="Email:" type="text" />
+      <Field name="name" component={renderInputField} placeholder="Your Name:" type="text" />
+      <Field name="telephone" component={renderInputField} placeholder="Telephone:" type="text" />
+      <Field name="email" component={renderInputField} placeholder="Email:" type="text" />
     </div>
     <div className="message">
-      <Field name="message" component="textarea" rows={8} placeholder="Message:" type="text" />
+      <Field name="message" component={renderTextAreaField} rows={8} placeholder="Message:" type="text" />
     </div>
-    <button className="action" type="submit" disabled={pristine || submitting}>Submit message</button>
+    <button className="action" type="submit" disabled={pristine || submitting || invalid}>Submit message</button>
   </form>
 );
 
+const renderInputField = (props: InputProps) => {
+  return (
+    <div className="input-field">
+      {props.meta.touched && props.meta.error && <div className="error">{props.meta.error}</div>}
+      <input placeholder={props.placeholder} {...props.input}/>
+    </div>
+  );
+};
+
+const renderTextAreaField = (props: InputProps) => {
+  return (
+    <>
+      {props.meta.touched && props.meta.error && <div className="error">{props.meta.error}</div>}
+      <textarea placeholder={props.placeholder} rows={props.rows} {...props.input}/>
+    </>
+  );
+};
+
 const validate = (values: ContactFormData): FormErrors<ContactFormData> => {
   return {
-    ...(!values.name ? {name: "Required"} : {}),
-    ...(!values.telephone ? {telephone: "Required"} : {}),
-    ...(!values.email ? {email: "Required"} : {}),
-    ...(values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email) ? {email: "Invalid email address"} : {}),
-    ...(!values.message ? {message: "Required"} : {})
+    ...(!values.name ? { name: 'Required' } : {}),
+    ...(!values.telephone ? { telephone: 'Required' } : {}),
+    ...(!values.email ? { email: 'Required' } : {}),
+    ...(values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email) ? { email: 'Invalid email address' } : {}),
+    ...(!values.message ? { message: 'Required' } : {}),
   };
 };
 
 export default reduxForm<ContactFormData, ContactFormProps>({
+  validate,
   form: 'contact',
   initialValues: initialState,
-  validate: validate
 })(contactForm);
