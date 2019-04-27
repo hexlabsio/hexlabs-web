2d4383060299098d5b592392ce624595d3916d6c
Gerard Downes <gdownes@gmail.com>
2019-04-27 13:06:05 +0100

    Adding deployment stack and cricleci instructions

---
+++ .circleci/stack/Stack.kt
@@ -0,0 +1,11 @@
+import io.hexlabs.kloudformation.module.s3.s3Website
+import io.kloudformation.KloudFormation
+import io.kloudformation.StackBuilder
+
+class Stack: StackBuilder {
+    override fun KloudFormation.create(args: List<String>) {
+        s3Website {
+            s3Bucket { bucketName(+"hexlabs-web") }
+        }
+    }
+}
